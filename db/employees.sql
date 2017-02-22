064996a402b86fcfaeaf48085754a9633b6c7c99
John Daly <jpdaly@gmail.com>
2017-02-22 20:42:39 +0000

    create job_titles table

--- db/employees.sql
+++ db/employees.sql
@@ -10,5 +10,12 @@ CREATE TABLE employees (
   ID INTEGER PRIMARY KEY AUTOINCREMENT,
   name VARCHAR(50),
   salary REAL,
-  department_id INTEGER references departments(id)
+  department_id INTEGER references departments(id),
+  job_title INTEGER references job_titles(id)
+
+);
+
+CREATE TABLE job_titles (
+ID INTEGER PRIMARY KEY AUTOINCREMENT,
+name VARCHAR(50)
 );
